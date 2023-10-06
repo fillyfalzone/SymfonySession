a43bbda13483f9c7d5948974f0d6b6e66bad6052
Arnaud Kameni <bekaman100@gmail.com>
2023-10-06 23:39:55 +0200

    add auth 1st part

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo', TextType::class, [
+                'attr' => ['class' =>'form-control']
+            ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' =>'form-control']
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false, // le champs password repeat ne sera pas stocké en bd
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field, form-control']],
+                'required' => true,
+                'first_options'  => ['label' => 'Password'],
+                'second_options' => ['label' => 'Repeat Password'],
+                // 'contraints' => [
+                //     new Regex([
+                //         'pattern' => '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/',
+                //         'message' => 'Le mot de passe doit contenir au moins une lettre majuscule, une lettre minuscule, un    chiffre et avoir une longueur d\'au moins 8 caractères.'])
+                //     ],
+            ]);
+                    
+       
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
